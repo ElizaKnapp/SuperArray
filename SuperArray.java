77121b027819818146991a444add6fdaa7fecd08
ElizaKnapp <eknapp20@stuy.edu>
2020-11-04 12:05:54 -0500

    changes made from beginning of class and added size to code

--- SuperArray.java
+++ SuperArray.java
@@ -1,6 +1,6 @@
 public class SuperArray {
   private String[] data;
-  private int size; //the current array size
+  private int size; //how many things- the current array size
 
   public SuperArray() {
     data = new String[10];
@@ -8,25 +8,22 @@ public class SuperArray {
   }
 
   public int size() {
-    int ans = 0;
-    for (int i = 0; i < data.length; i++) {
-      if (data[i] == null) i = data.length;
-      else ans++;
-    }
-    return ans;
+    return size;
   }
 
   public boolean add(String element) {
-    if (size() < data.length) {
-        data[size()] = element;
+    if (size < data.length) {
+        data[size] = element;
+        size++;
         return true;
     }
-    else if (size() >= data.length){
+    else if (size >= data.length){
       resize();
       add(element);
       return true;
     }
     else return false;
+
   }
 
   public String get(int index) {
@@ -40,10 +37,37 @@ public class SuperArray {
   }
 
   private void resize() {
-    String[] newArray = new String[data.length + 10];
+    String[] newArray = new String[data.length * 2 + 1];
     for (int i = 0; i < data.length; i++) {
       newArray[i] = data[i];
     }
     data = newArray;
+  }
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public void clear() {
+    data = new String[data.length];
+    size = 0;
+  }
+
+  public String toString() {
+    String ans = "[";
+    for (int i = 0; i < size() - 1; i++) {
+      ans += get(i) + ", ";
+    }
+    ans = ans + get(size() - 1) + "]";
+    return ans;
+  }
+
+  public boolean contains(String s) {
+    for (int i = 0; i < size(); i++) {
+      if (data[i] == s) {
+        return true;
+      }
+    }
+    return false;
   }
 }
