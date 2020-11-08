41b2923ee8c183cd66dc9b1160adc09b30861f17
ElizaKnapp <eknapp20@stuy.edu>
2020-11-07 21:45:48 -0500

    ran my own tests, and reng's tests, and k's tests. Everything is working i think

--- Demo.java
+++ Demo.java
@@ -1,6 +1,15 @@
 public class Demo {
   public static void main(String[] args) {
+    SuperArray words = new SuperArray();
+       //grouped to save vertical space
+       words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+       words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+       words.add("una");    words.add("ebi");     words.add("toro");
 
+       System.out.println(words);
+    //   removeDuplicates(words);
+    //   System.out.println(words);
+      System.out.println(words.lastIndexOf("ebi"));
   }
   public static void removeDuplicates(SuperArray s) {
     for (int i = s.size() - 1; i >= 0; i--) {
