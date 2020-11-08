8fb70f3315dff7f6bdba65b86f598a0714b28839
ElizaKnapp <eknapp20@stuy.edu>
2020-11-07 20:46:39 -0500

    removeDuplicates done

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+  public static void main(String[] args) {
+
+  }
+  public static void removeDuplicates(SuperArray s) {
+    for (int i = s.size() - 1; i >= 0; i--) {
+      if (s.indexOf(s.get(i)) != i) s.remove(i);
+    }
+  }
+}
